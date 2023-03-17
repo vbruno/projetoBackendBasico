c37781f2c8f5d92aeacaffe5a8bb4da0f970e363
Bruno S Velho <bruno.velho@gmail.com>
2023-03-17 11:48:16 -0300

    Refatorando código por causa da separação das rotas

--- src/router/route.user.ts
+++ src/router/route.user.ts
@@ -1,9 +1,64 @@
 import { Router } from "express";
+import { randomUUID } from "node:crypto";
+import { Database } from "../database";
+
+interface IDatabase {
+  msg?: string;
+}
 
 const userRoutes = Router();
 
+const database = new Database();
+
+const tableUser = "user";
+
 userRoutes.get("/", (request, response) => {
-  response.send("Acessando rota user");
+  const user = database.select(tableUser);
+  response.json(user);
+});
+
+userRoutes.get("/:id", (request, response) => {
+  const { id } = request.params;
+
+  const user = database.select(tableUser, id);
+  response.json(user);
+});
+
+// Parâmetro que esta vindo do CLIENTE - REQUEST
+// Parâmetro que esta indo para o CLIENTE - RESPONSE
+
+userRoutes.post("/", (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    id: randomUUID(),
+    name: name,
+    email,
+  };
+
+  database.insert(tableUser, user);
+
+  response.status(201).json({ msg: "sucesso!" });
+});
+
+userRoutes.delete("/:id", (request, response) => {
+  const { id } = request.params;
+
+  if (id.length === 36) {
+    let userExist = database.select(tableUser, id);
+
+    if (JSON.stringify(userExist) === "[]") {
+      return response.send("Usuário não encontrado");
+    }
+
+    database.delete(tableUser, id);
+
+    return response.json({
+      msg: `${userExist[0].name} foi deletado`,
+      userExist,
+    });
+  }
+  response.send("Erro de requisição");
 });
 
 export { userRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { randomUUID } from "node:crypto";
-import { Database } from "./database";
 
 import { router } from "./router/index";
 
@@ -11,53 +9,6 @@ const port = 3333;
 server.use(express.json());
 
 server.use(router);
-
-const database = new Database();
-
-const tableUser = "user";
-
-server.get("/", (request, response) => {
-  const user = database.select(tableUser);
-  response.json(user);
-});
-
-server.get("/:id", (request, response) => {
-  const { id } = request.params;
-
-  const user = database.select(tableUser, id);
-  response.json(user);
-});
-
-// Parâmetro que esta vindo do CLIENTE - REQUEST
-// Parâmetro que esta indo para o CLIENTE - RESPONSE
-
-server.post("/", (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    id: randomUUID(),
-    name: name,
-    email,
-  };
-
-  database.insert(tableUser, user);
-
-  response.status(201).json({ msg: "sucesso!" });
-});
-
-server.delete("/:id", (request, response) => {
-  const { id } = request.params;
-
-  if (id.length === 36) {
-    const userExist = database.select(tableUser, id);
-
-    if (JSON.stringify(userExist) === "[]") {
-      return response.send("Usuário não encontrado");
-    }
-    return response.json(userExist);
-  }
-  response.send("Erro de requisição");
-});
 
 server.listen(port, () => {
   console.log(`Server Running - end: http://localhost:${port}`);
